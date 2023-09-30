666fb4d8914b0d316971e17c8cf0942ab199eda5
PrathamSahani <prathams0368@gmail.com>
2023-09-30 23:06:01 +0530

    Django

--- resumeuploader/resumeuploader/settings.py
+++ resumeuploader/resumeuploader/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-==7k=1gv@o=f*lr-wcaaq9%6!pl4l$q(a2=8izew#l0fdxvxh5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -118,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
